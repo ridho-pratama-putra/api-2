451f34e1f430e01b16c7c93ff9cf51c723f02944
ridho <ridho.putra@btpn.com>
2022-04-09 01:08:27 +0700

    package os

--- example/stageThree/stageThree.go
+++ example/stageThree/stageThree.go
@@ -8,14 +8,14 @@ import (
 )
 
 func main() {
-	fmt.Println("/nPACKAGE & IMPORTS")
+	fmt.Println("\nPACKAGE & IMPORTS")
 	fmt.Println("stage three", utils.AlayString("Ini nih "))
 	fmt.Println("stage three", utils.AreOfCircleWithRadius(5.1))
 
-	fmt.Println("/nACCESS MODIFIER")
+	fmt.Println("\nACCESS MODIFIER")
 	fmt.Println("accessing value defined in twoDimension", utils.PI)
 
-	fmt.Println("/nPACKAGE OS")
+	fmt.Println("\nPACKAGE OS")
 	// go run example/stageThree/stageThree.go arema indonesia
 	fmt.Println("args 0, command itu sendiri :", os.Args[0]) //  /var/folders/ng/4wp9dvzn65lf1nbvhc38wt0cnwlks5/T/go-build1796574156/b001/exe/stageThree
 	//fmt.Println("apa sih ini :", os.Args[1]) // arema
