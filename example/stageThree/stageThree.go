54772f8619cb6834b288eaa0458f692c0785566d
ridho <ridho.putra@btpn.com>
2022-04-18 11:40:11 +0700

    package date

--- example/stageThree/stageThree.go
+++ example/stageThree/stageThree.go
@@ -12,6 +12,7 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -141,6 +142,38 @@ func main() {
 
 	sort.Sort(UserSlice(users))
 	fmt.Println(users)
+
+	fmt.Println("\nPACKAGE time")
+	//ANSIC			"Mon Jan _2 15:04:05 2006"
+	//UnixDate		"Mon Jan _2 15:04:05 MST 2006"
+	//RubyDate		"Mon Jan 02 15:04:05 -0700 2006"
+	//RFC822		"02 Jan 06 15:04 MST"
+	//RFC822Z		"02 Jan 06 15:04 -0700"
+	//RFC850		"Monday, 02-Jan-06 15:04:05 MST"
+	//RFC1123		"Mon, 02 Jan 2006 15:04:05 MST"
+	//RFC1123Z		"Mon, 02 Jan 2006 15:04:05 -0700"
+	//RFC3339		"2006-01-02T15:04:05Z07:00"
+	//RFC3339Nano	"2006-01-02T15:04:05.999999999Z07:00"
+	//Stamp			"3:04PM"
+	//StampMilli	"Jan _2 15:04:05.000"
+	//StampMicro	"Jan _2 15:04:05.000000"
+	//StampNano		"Jan _2 15:04:05.000000000"
+	location, _ := time.LoadLocation("Asia/Jakarta")
+	fmt.Println("full", time.Now().In(location))
+	fmt.Println("years", time.Now().Year())
+	fmt.Println("month", time.Now().Month())
+	fmt.Println("day", time.Now().Day()) // tanggal
+	fmt.Println("day", time.Now().Weekday()) // hari
+	fmt.Println("hour", time.Now().Hour())
+	fmt.Println("minute", time.Now().Minute())
+	fmt.Println("second", time.Now().Second())
+	fmt.Println("create date UTC", time.Date(2009, time.November, 11, 19, 0, 0, 0, time.UTC))
+	parsedTime, _ := time.Parse(time.RFC3339, "2006-01-31T15:04:55Z")
+	fmt.Println("parse date UTC using RFC3339", parsedTime)
+
+	rfc339DateFormat := "2006-01-02"
+	parsedTimeCustom, _ := time.Parse(rfc339DateFormat, "2020-05-30")
+	fmt.Println("parse date UTC using custom format", parsedTimeCustom)
 }
 
 type User struct {
